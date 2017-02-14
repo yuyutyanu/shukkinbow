5e7649e91806258e0ca9b34e8a867f54d00e551a
chanu <chanu@chanu-no-MacBook-Pro.local>
2017-02-14 17:47:25 +0900

    viewport追加

--- resources/views/blade_template.blade.php
+++ resources/views/blade_template.blade.php
@@ -4,6 +4,7 @@
     <link rel="stylesheet" href="/css/template.css">
     <link rel="stylesheet" href="/css/reset.css">
     <link rel="stylesheet" href="/css/@yield('css')">
+    <meta name="viewport" content="width=device-width,initial-scale=1.0,minimum-scale=1.0">
 </head>
 <body>
 <div class="background">
