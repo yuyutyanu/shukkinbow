d4ae2f9e4c6d0b5863f1970b3d121a2bd0f68df0
chanu <chanu@chanu-no-MacBook-Pro.local>
2017-03-13 23:35:16 +0900

    styleのズレ解消

--- resources/views/end.blade.php
+++ resources/views/end.blade.php
@@ -5,7 +5,7 @@
 
 @section('top')
     <div class="time-wrapper">
-        <p>開始時間- {{ $attendance_info->start_time }}</p>
+        <p>開始時間 - {{ $attendance_info->start_time }}</p>
         <p>終了時間 - {{ $attendance_info->end_time }}</p>
         <p>勤務時間 - {{ $attendance_info->working_time }}</p>
     </div>
