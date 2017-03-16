cdacc628653aeb2a2c9f80bb9f236ea631eb626c
chanu <yuyutyanu@yahoo.co.jp>
2017-03-17 07:54:09 +0900

    一つ前の変更を元に戻した

--- app/Http/Controllers/GoogleAppScriptController.php
+++ app/Http/Controllers/GoogleAppScriptController.php
@@ -17,7 +17,6 @@ class GoogleAppScriptController extends Controller
 
             // userごとの勤務情報を取得  (実装時は先月の情報を取りたい)
             $recodes = $user->attendancerecord()
-                ->orderBy('start_time','desc')
                 ->get();
 
 
