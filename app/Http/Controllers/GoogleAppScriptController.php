7f96202d8775cc06ca52cccce29e6ee19f26d649
chanu <yuyutyanu@yahoo.co.jp>
2017-03-17 16:01:23 +0900

    データが上書きされるバグ修正

--- app/Http/Controllers/GoogleAppScriptController.php
+++ app/Http/Controllers/GoogleAppScriptController.php
@@ -12,13 +12,12 @@ class GoogleAppScriptController extends Controller
 
         $users = $user->get();
         $attendance = [];
-
+        $count = 0;
         foreach ($users as $user) {
 
             // userごとの勤務情報を取得  (実装時は先月の情報を取りたい)
             $recodes = $user->attendancerecord()
                 ->get();
-
 
             $name = $user->name;
 
@@ -29,12 +28,13 @@ class GoogleAppScriptController extends Controller
                 $end_datetime = Carbon::parse($recode->end_time);
                 $work_location = $work_location->where('id',$recode->location_id)->first();
 
-                $attendance[$index]["name"] = $name;
-                $attendance[$index]["location"] = $work_location->location;
-                $attendance[$index]["start_day"] = $start_datetime->day;
-                $attendance[$index]["start_month"] = $start_datetime->month;
-                $attendance[$index]["start_time"] = $start_datetime->hour.":".$start_datetime->minute.":".$start_datetime->second;
-                $attendance[$index]["end_time"] = $end_datetime->hour.":".$end_datetime->minute.":".$end_datetime->second;
+                $attendance[$count]["name"] = $name;
+                $attendance[$count]["location"] = $work_location->location;
+                $attendance[$count]["start_day"] = $start_datetime->day;
+                $attendance[$count]["start_month"] = $start_datetime->month;
+                $attendance[$count]["start_time"] = $start_datetime->hour.":".$start_datetime->minute.":".$start_datetime->second;
+                $attendance[$count]["end_time"] = $end_datetime->hour.":".$end_datetime->minute.":".$end_datetime->second;
+                $count++;
             }
         }
         return $attendance;
